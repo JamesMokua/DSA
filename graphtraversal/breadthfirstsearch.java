af41ea5d9849679cedcccc2511e254fd996bc7d4
James Mokua <jamieedgar02@gmail.com>
2022-12-26 19:39:34 +0300

    breadth first search adjacency matrix

---
+++ graphtraversal/breadthfirstsearch.java
@@ -0,0 +1,107 @@
+package graphtraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class breadthfirstsearch {
+    public static class GraphNode {
+        public String name;
+        public int index;
+        public boolean isVisited = false;
+
+        public GraphNode(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
+    }
+
+    ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+    int[][] adjacencyMatrix;
+
+    public breadthfirstsearch(ArrayList<GraphNode> nodeList) {
+        this.nodeList = nodeList;
+        adjacencyMatrix = new int[nodeList.size()][nodeList.size()];
+    }
+
+    public void addUndirectedEdge(int i, int j) {
+        adjacencyMatrix[i][j] = 1;
+        adjacencyMatrix[j][i] = 1;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("   ");
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + " ");
+        }
+        s.append("\n");
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + ": ");
+            for (int j : adjacencyMatrix[i]) {
+                s.append((j) + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    // get neighbours
+    public ArrayList<GraphNode> getNeighbours(GraphNode node) {
+        // taking node index and checking if any columns have the node
+        ArrayList<GraphNode> neighbours = new ArrayList<GraphNode>();
+        int nodeIndex = node.index;
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[nodeIndex][i] == 1) {
+                neighbours.add(nodeList.get(i));
+            }
+        }
+        return neighbours;
+    }
+
+    // BFS Internal
+    public void bfsVisit(GraphNode node) {
+        LinkedList<GraphNode> queue = new LinkedList<GraphNode>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove(0);
+            currentNode.isVisited = true;
+            System.out.print(currentNode.name + " ");
+            ArrayList<GraphNode> neighbours = getNeighbours(currentNode);
+            for (GraphNode neighbour : neighbours) {
+                if (!neighbour.isVisited) {
+                    queue.add(neighbour);
+                    neighbour.isVisited = true;
+                }
+            }
+        }
+    }
+
+    // BFS
+    public void bfs() {
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited) {
+                bfsVisit(node);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+        nodeList.add(new GraphNode("A", 0));
+        nodeList.add(new GraphNode("B", 1));
+        nodeList.add(new GraphNode("C", 2));
+        nodeList.add(new GraphNode("D", 3));
+        nodeList.add(new GraphNode("E", 4));
+        breadthfirstsearch bfs = new breadthfirstsearch(nodeList);
+        bfs.addUndirectedEdge(0, 1);
+        bfs.addUndirectedEdge(0, 2);
+        bfs.addUndirectedEdge(0, 3);
+        bfs.addUndirectedEdge(1, 4);
+        bfs.addUndirectedEdge(2, 3);
+        bfs.addUndirectedEdge(3, 4);
+        System.out.println(bfs.toString());
+
+        bfs.bfs();
+
+    }
+}
